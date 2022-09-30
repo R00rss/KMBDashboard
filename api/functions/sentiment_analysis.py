e0db36176eadf0bca8b0ed737b17ab344c2471b9
RONNY RICARDO GARCIA PALACIOS <rgarcia@KMB.UIO>
2022-09-30 13:23:59 -0500

    first commit

---
+++ api/functions/sentiment_analysis.py
@@ -0,0 +1,9 @@
+import nltk
+# from googletrans import Translator
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import translators as ts
+
+sid = SentimentIntensityAnalyzer()
+
+def get_polarity_from_string(sentence):
+    return sid.polarity_scores(ts.google(sentence , to_language = 'en'))
